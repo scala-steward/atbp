f3089508ca9c86390d677cd63a761d4b9ce8c0aa
Scala Steward <scala_steward@virtuslab.com>
2025-03-05 17:07:04 +0000

    Update zio-schema-json to 1.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
     val zioLogging = "dev.zio" %% "zio-logging" % ZioLogging
     val zioLoggingSlf4j2 = "dev.zio" %% "zio-logging-slf4j2" % ZioLogging
 
-    val zioSchemaJson = "dev.zio" %% "zio-schema-json" % "1.6.3"
+    val zioSchemaJson = "dev.zio" %% "zio-schema-json" % "1.6.4"
 
     object TestLibs {
       val pprint = Libs.pprint % Test
