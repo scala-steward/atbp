8b7f8209930ae08ab4fbe0397927a11f90e660a6
Edward Samson <edward@samson.ph>
2025-06-01 21:21:57 +0800

    feat(inspector): filter recent changelogs excluding rank-only changes
    Update changelog check to consider only those created within the last
    14 days that include changes beyond just the "Rank" field. This ensures
    the inspector focuses on meaningful updates rather than trivial rank
    adjustments.

--- plate/src/main/scala/ph/samson/atbp/plate/Inspector.scala
+++ plate/src/main/scala/ph/samson/atbp/plate/Inspector.scala
@@ -2,6 +2,7 @@ package ph.samson.atbp.plate
 
 import better.files.File
 import ph.samson.atbp.jira.Client
+import ph.samson.atbp.jira.model.Changelog
 import ph.samson.atbp.jira.model.Issue
 import ph.samson.atbp.plate.JiraOps.*
 import zio.Clock
@@ -92,7 +93,11 @@ object Inspector {
       for {
         changelogs <- client.getChangelogs(issue.key)
       } yield {
-        changelogs.exists(_.created.isAfter(ZonedDateTime.now().minusDays(14)))
+        changelogs.exists {
+          case Changelog(id, author, created, items, historyMetadata) =>
+            created.isAfter(ZonedDateTime.now().minusDays(14)) &&
+            !items.forall(_.field == "Rank")
+        }
       }
     }
 
