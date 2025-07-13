0c849dcd94012e3bf78cfb4a71a46bb38897febc
Scala Steward <scala_steward@virtuslab.com>
2025-07-13 14:22:04 +0000

    Update zio-logging, zio-logging-slf4j2 to 2.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val Zio = "2.1.19"
     val ZioConfig = "4.0.4"
     val ZioHttp = "3.3.3"
-    val ZioLogging = "2.5.0"
+    val ZioLogging = "2.5.1"
   }
 
   object Libs {
