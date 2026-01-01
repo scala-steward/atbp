b0b48c62721f7bfdb3a6d652833115730971129d
Scala Steward <scala_steward@virtuslab.com>
2026-01-01 16:08:59 +0000

    Update jsoup to 1.22.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
 
     val commonsIo = "commons-io" % "commons-io" % "2.21.0"
 
-    val jsoup = "org.jsoup" % "jsoup" % "1.21.2"
+    val jsoup = "org.jsoup" % "jsoup" % "1.22.1"
 
     val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2025.10"
 
