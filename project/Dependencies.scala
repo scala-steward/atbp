9c05ca099b1530297696d44e827a0c7ea8fa51ce
Scala Steward <scala_steward@virtuslab.com>
2026-05-11 18:27:26 +0000

    Update zio-http to 3.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val CommonMark = "0.28.0"
     val Zio = "2.1.25"
     val ZioConfig = "4.0.7"
-    val ZioHttp = "3.11.0"
+    val ZioHttp = "3.11.1"
     val ZioLogging = "2.5.3"
   }
 
