d8806288b332b669109b9ab992dd8fd268693832
Edward Samson <edward@samson.ph>
2026-07-04 06:26:33 +0800

    Use default glicko tuning DRY

--- liga/src/main/scala/ph/samson/atbp/liga/glicko/Tuning.scala
+++ liga/src/main/scala/ph/samson/atbp/liga/glicko/Tuning.scala
@@ -5,12 +5,5 @@ import dimos.glicko2.Tuning as GlickoTuning
 /** Repo-wide Glicko2 tuning (1500 / 350 / 0.06 / τ 0.5). */
 object Tuning {
 
-  val Default: GlickoTuning = GlickoTuning.default(
-    initRating = 1500,
-    maxDeviation = 350,
-    minDeviation = 0,
-    initVolatility = 0.06,
-    tau = 0.5,
-    tolerance = 0.000001
-  )
+  val Default: GlickoTuning = GlickoTuning.default()
 }
