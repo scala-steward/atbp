3cbb467e12ad2cebda428c4a6265122325628194
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 15:56:50 +0000

    Update zio-json to 1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
 
     val zioHttp = "dev.zio" %% "zio-http" % ZioHttp
 
-    val zioJson = Def.setting("dev.zio" %%% "zio-json" % "0.10.0")
+    val zioJson = Def.setting("dev.zio" %%% "zio-json" % "1.1.0")
 
     val zioTestJs = Def.setting("dev.zio" %%% "zio-test" % Zio % Test)
     val zioTestSbtJs = Def.setting("dev.zio" %%% "zio-test-sbt" % Zio % Test)
