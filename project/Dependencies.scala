41b6a24553061b789da88954f9a428f400abf4cf
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 15:56:19 +0000

    Update zio-config, zio-config-magnolia, ... to 4.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val CommonMark = "0.30.0"
     val D2 = "v0.9.0"
     val Zio = "2.1.26"
-    val ZioConfig = "4.0.8"
+    val ZioConfig = "4.1.0"
     val ZioHttp = "3.11.4"
     val ZioLogging = "2.5.3"
   }
